5ece93675079b1632b12769bdf21679ba846694f
BlivionIaG <kev29lt@gmail.com>
2020-04-13 18:25:39 +0200

    Base File

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <router-link to="/" class="d-flex align-center">
+        <v-icon
+          class="shrink mr-2"
+          contain
+          transition="scale-transition"
+          width="40"
+          >mdi-map</v-icon
+        >
+
+        <span id="headerLogo">
+          Kartenn
+        </span>
+      </router-link>
+
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <v-icon>mdi-invert-colors</v-icon>
+      </v-btn>
+      <router-link to="/login">
+        <span class="mr-2">Connexion</span>
+        <v-icon>mdi-login</v-icon>
+      </router-link>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({})
+};
+</script>
+
+<style lang="less">
+#headerLogo {
+  font-size: 2em;
+  font-style: bold;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,94 @@
+    <template>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-toolbar color="primary" dark flat>
+                  <v-toolbar-title>Login form</v-toolbar-title>
+                  <v-spacer />
+                </v-toolbar>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      v-model="user.email"
+                      prepend-icon="mdi-at"
+                      label="Email"
+                      type="email"
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="user.password"
+                      prepend-icon="mdi-textbox-password"
+                      label="Password"
+                      type="password"
+                    ></v-text-field>
+                    <v-text-field
+                      v-if="!options.isLoggingIn"
+                      v-model="user.confirmPassword"
+                      prepend-icon="mdi-textbox-password"
+                      label="Confirm password"
+                      type="password"
+                    ></v-text-field>
+                    <v-checkbox
+                      v-if="options.isLoggingIn"
+                      v-model="options.shouldStayLoggedIn"
+                      label="Stay logged in?"
+                      hide-details="hide-details"
+                    ></v-checkbox>
+
+                    <v-btn
+                      v-if="options.isLoggingIn"
+                      @click.prevent=""
+                      block="block"
+                      type="submit"
+                      flat
+                      color="primary"
+                      >SIGN IN</v-btn
+                    >
+                    <v-btn
+                      v-else
+                      @click.prevent=""
+                      block="block"
+                      type="submit"
+                      flat
+                      color="primary"
+                      >SIGN UP</v-btn
+                    >
+                  </v-form>
+                </v-card-text>
+                <v-card-subtitle align="center">
+                  <div v-if="options.isLoggingIn">
+                    <span class="mr-2">Don't have an account?</span>
+                    <v-btn class="mr-2" @click="options.isLoggingIn = false"
+                      >SIGN UP</v-btn
+                    >
+                  </div>
+                  <div v-else>
+                    <span class="mr-2">Already have an account?</span>
+                    <v-btn class="mr-2" @click="options.isLoggingIn = true"
+                      >SIGN IN</v-btn
+                    >
+                  </div>
+                </v-card-subtitle>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </template>
+
+    <script>
+    export default {
+      data: () => ({
+        user: {
+          // email: 'admin@example.com',
+          // password: 'admin',
+          // name: 'John Doe',
+        },
+        options: {
+          isLoggingIn: true,
+          shouldStayLoggedIn: true
+        }
+      })
+    }
+    </script>
