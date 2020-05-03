838d36d466ed71b9b661e7a4d8620cb375a8fe52
BlivionIaG <kev29lt@gmail.com>
2020-05-03 03:06:35 +0200

    More secure

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export default {
-    api_url: "https://kartenn.chenco.tk:8420/",
+    api_url: "https://kartenn.chenandco.tk:8420/",
     tile_server: "https://kartenn.chenco.tk/",
     township_geojson: "wsg84fix.bretagne.geojson.min.json"
 };
--- src/lib/http-common.js
+++ src/lib/http-common.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import constants from "../constants";
 
-export default axios.create({
+const http = axios.create({
   baseURL: constants.api_url,
   headers: {
-    "Content-type": "application/json",
-    "x-access-token": localStorage.getItem('xtoken'),
+    "Content-type": "application/json"
   }
-});
+})
+
+http.interceptors.request.use(
+  async (config) => {
+    const token = await localStorage.getItem('xtoken')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+export default http
